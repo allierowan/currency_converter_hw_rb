e8c5b50e00cf71fd1d917887894a1027b73664bf
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-14 13:39:38 -0400

    created empty methods for each of the things a currency needs to be able to do

--- currency.rb
+++ currency.rb
@@ -1,2 +1,31 @@
 class Currency
+
+  # initialize currency object with an amount and a currency code
+  def initialize(amount, currency_code)
+    @amount = amount
+    @currency_code = currency_code
+  end
+
+  # two currency objects should equal each other if and only if both the amount and the currency code are the same
+  def ==(other_currency)
+
+  end
+
+  # a currency object should be able to be added to another one with the same code
+  # should raise a DifferentCurrencyCodeError when code is different
+  def +(other_currency)
+
+  end
+
+  # a currency object should be able to be subtracted from another one with the same code
+  # should raise a DifferentCurrencyCodeError when code is different
+  def -(other_currency)
+
+  end
+
+  # when multiplied by a fixnum or a float, should return a currency object
+  def *(number)
+
+  end
+
 end
