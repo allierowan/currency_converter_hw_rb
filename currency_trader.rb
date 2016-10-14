fc5cfc2c9a379c92493143998f45c402bbc9a56f
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-14 15:49:01 -0400

    created the initialize method

--- currency_trader.rb
+++ currency_trader.rb
@@ -1,3 +1,11 @@
 class CurrencyTrader
-  
+
+  # currency trader object should be initialized with two currency converter objects from 2 diff points in time
+  def initialize(converter1, converter2, start_currency)
+    @converter1 = converter1
+    @converter2 = converter2
+    @start_currency = start_currency
+    
+  end
+
 end
