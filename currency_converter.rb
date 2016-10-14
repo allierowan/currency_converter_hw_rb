422465ee355344c5a9ccf2d1a7048d6eba223d85
Aileen Rowan <aileen.s.rowan@gmail.com>
2016-10-14 14:45:40 -0400

    created convert method and tested it

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,5 +1,20 @@
+require_relative "./currency"
+
 class CurrencyConverter
 
-  
+  # should be initialized with a hash of currency codes and conversion rates
+  # i.e. {"USD" => 1.0, "EUR" => 0.74}
+  def initialize(rates_lookup)
+    @rates_lookup = rates_lookup
+  end
 
+  # method to grab the rates_lookup hash
+  def rates_lookup
+    @rates_lookup
+  end
+
+  # given a currency object, should be able to return a new currency object in the requested currency code
+  def convert(currency, code)
+    currency * (rates_lookup[code] / rates_lookup[currency.code])
+  end
 end
--- currency_playground.rb
+++ currency_playground.rb
@@ -1,17 +1,20 @@
 require_relative "./currency"                       # => true
 require_relative "./different_currency_code_error"  # => true
+require_relative "./currency_converter"             # => true
 
-ten_dollars = Currency.new(10, "USD")     # => #<Currency:0x007fe13789ab28 @amount=10, @code="USD">
-thirty_dollars = Currency.new(30, "USD")  # => #<Currency:0x007fe13789a588 @amount=30, @code="EUR">
+ten_dollars = Currency.new(10, "USD")     # => #<Currency:0x007f8ae28421b8 @amount=10, @code="USD">
+thirty_dollars = Currency.new(30, "USD")  # => #<Currency:0x007f8ae2840e80 @amount=30, @code="USD">
+one_eur = Currency.new(1, "EUR")          # => #<Currency:0x007f8ae2840908 @amount=1, @code="EUR">
 
 thirty_dollars == ten_dollars  # => false
 
-forty_dollars = ten_dollars + thirty_dollars  # ~> DifferentCurrencyCodeError: currency codes are different
+forty_dollars = ten_dollars + thirty_dollars  # => #<Currency:0x007f8ae283bef8 @amount=40, @code="USD">
 
-twenty_dollars = thirty_dollars - ten_dollars  # ~> DifferentCurrencyCodeError: currency codes are different
+twenty_dollars = thirty_dollars - ten_dollars  # => #<Currency:0x007f8ae283b958 @amount=20, @code="USD">
 
-# ~> DifferentCurrencyCodeError
-# ~> currency codes are different
-# ~>
-# ~> /Users/aileenrowan/code/currency_converter_hw_rb/currency.rb:35:in `-'
-# ~> /Users/aileenrowan/code/currency_converter_hw_rb/currency_playground.rb:11:in `<main>'
+fifty_dollars = ten_dollars*5  # => #<Currency:0x007f8ae283b480 @amount=50, @code="USD">
+
+currency_converter = CurrencyConverter.new({"USD" => 1.0, "EUR" => 0.74, "JPY" => 120.0})  # => #<CurrencyConverter:0x007f8ae283ae18 @rates_lookup={"USD"=>1.0, "EUR"=>0.74, "JPY"=>120.0}>
+
+currency_converter.convert(ten_dollars, "EUR")  # => #<Currency:0x007f8ae283a3a0 @amount=7.4, @code="USD">
+currency_converter.convert(one_eur, "JPY")      # => #<Currency:0x007f8ae2839888 @amount=162.16216216216216, @code="EUR">
